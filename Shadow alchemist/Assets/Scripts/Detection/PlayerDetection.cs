42acf6695f34485e49fada57c6efda01c0864581
Limuwahanelis <37908839+Limuwahanelis@users.noreply.github.com>
2024-07-24 01:51:28 +0200

    Import detection system

---
+++ Shadow alchemist/Assets/Scripts/Detection/PlayerDetection.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.Events;
+
+public class PlayerDetection : MonoBehaviour
+{
+    public UnityEvent OnPlayerDetectedUnity;
+    public UnityEvent OnPlayerLeftdUnity;
+    public Action OnPlayerDetected;
+    public Action OnPlayerLeft;
+    public Vector3 playerPos;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        OnPlayerDetected?.Invoke();
+        OnPlayerDetectedUnity?.Invoke();
+        playerPos = collision.transform.position;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        playerPos = Vector3.zero;
+        OnPlayerLeftdUnity?.Invoke();
+        OnPlayerLeft?.Invoke();
+    }
+}
