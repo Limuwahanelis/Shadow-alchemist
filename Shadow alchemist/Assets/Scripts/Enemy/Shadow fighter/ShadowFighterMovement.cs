3eaebd0eaffacf6898e3cff7b563e8171ad7bea4
Limuwahanelis <37908839+Limuwahanelis@users.noreply.github.com>
2024-07-25 17:10:36 +0200

    Add FlipSide to shadow fighter movement

--- Shadow alchemist/Assets/Scripts/Enemy/Shadow fighter/ShadowFighterMovement.cs
+++ Shadow alchemist/Assets/Scripts/Enemy/Shadow fighter/ShadowFighterMovement.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class ShadowFighterMovement : MonoBehaviour
 {
+
+    public GlobalEnums.HorizontalDirections FlipSide => (GlobalEnums.HorizontalDirections)_flipSide;
     [SerializeField] Rigidbody2D _rb;
     [SerializeField] float _speed;
     [SerializeField] ShadowFighterController _shadowFighter;
-    //private int _flipSide = 1;
+    private int _flipSide = 1;
     Vector3 _mainbodyScale;
     public void Move(Vector2 direction)
     {
@@ -17,6 +19,7 @@ public class ShadowFighterMovement : MonoBehaviour
             _mainbodyScale.y = _shadowFighter.MainBody.transform.localScale.y;
             _mainbodyScale.z = _shadowFighter.MainBody.transform.localScale.z;
             _shadowFighter.MainBody.transform.localScale = _mainbodyScale;
+            _flipSide = 1;
         }
         else
         {
@@ -24,6 +27,7 @@ public class ShadowFighterMovement : MonoBehaviour
             _mainbodyScale.y = _shadowFighter.MainBody.transform.localScale.y;
             _mainbodyScale.z = _shadowFighter.MainBody.transform.localScale.z;
             _shadowFighter.MainBody.transform.localScale = _mainbodyScale;
+            _flipSide = -1;
         }
         _rb.MovePosition(_rb.position + direction * _speed * Time.deltaTime);
     }
