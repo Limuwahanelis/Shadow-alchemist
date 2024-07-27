f1b09ba810b2ede497dbd4a4db5e49787c203e22
Limuwahanelis <37908839+Limuwahanelis@users.noreply.github.com>
2024-07-27 23:26:50 +0200

    Add spikes script

---
+++ Shadow alchemist/Assets/Scripts/Obstacles/Spike.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spike : MonoBehaviour
+{
+    int dmg = 16;
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        IDamagable player = collision.transform.GetComponent<PlayerHealthSystem>();
+        IPushable toPush = collision.transform.GetComponent<PlayerHealthSystem>();
+        toPush.Push(PlayerHealthSystem.DamageType.TRAPS);
+        player.TakeDamage(new DamageInfo(dmg,HealthSystem.DamageType.TRAPS,transform.position));
+    }
+}
