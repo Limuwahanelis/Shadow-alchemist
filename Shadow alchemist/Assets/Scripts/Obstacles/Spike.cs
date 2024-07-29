009a755f6f020ddccb30c1417f37e11a26d3dd86
Limuwahanelis <37908839+Limuwahanelis@users.noreply.github.com>
2024-07-29 23:25:55 +0200

    Update Spike.cs

--- Shadow alchemist/Assets/Scripts/Obstacles/Spike.cs
+++ Shadow alchemist/Assets/Scripts/Obstacles/Spike.cs
@@ -9,7 +9,7 @@ public class Spike : MonoBehaviour
     {
         IDamagable player = collision.transform.GetComponent<PlayerHealthSystem>();
         IPushable toPush = collision.transform.GetComponent<PlayerHealthSystem>();
-        toPush.Push(PlayerHealthSystem.DamageType.TRAPS);
-        player.TakeDamage(new DamageInfo(dmg,HealthSystem.DamageType.TRAPS,transform.position));
+        if(toPush!=null) toPush.Push(PlayerHealthSystem.DamageType.TRAPS);
+        if(player!=null)player.TakeDamage(new DamageInfo(dmg,HealthSystem.DamageType.TRAPS,transform.position));
     }
 }
